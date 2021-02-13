c0afd77206e68a29c1e53f29408629d38608776b
Tarun Patel <ptarun@cloudhealthtech.com>
2021-02-13 16:41:37 +0000

    More HTML to GO refactoring

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -1,12 +1,14 @@
 package handlers
 
 import (
+	"encoding/json"
+	"fmt"
 	"github.com/tpatel84/golang-booking-web-dev/pkg/config"
 	"github.com/tpatel84/golang-booking-web-dev/pkg/models"
 	"github.com/tpatel84/golang-booking-web-dev/pkg/renders"
+	"log"
 	"net/http"
 )
-
 
 // Repo the repository used the handlers
 var Repo *Repository
@@ -32,7 +34,7 @@ func (rp *Repository) Home(w http.ResponseWriter, r *http.Request) {
 	remoteIP := r.RemoteAddr
 	rp.App.Session.Put(r.Context(), "remote_ip", remoteIP)
 
-	renders.RenderTemplate(w, "home.page.tmpl", &models.TemplateData{})
+	renders.RenderTemplate(w, r, "home.page.tmpl", &models.TemplateData{})
 }
 
 func (rp *Repository) About(w http.ResponseWriter, r *http.Request) {
@@ -42,27 +44,62 @@ func (rp *Repository) About(w http.ResponseWriter, r *http.Request) {
 	remoteIP := rp.App.Session.GetString(r.Context(), "remote_ip")
 	stringMap["remote_ip"] = remoteIP
 
-	renders.RenderTemplate(w, "about.page.tmpl", &models.TemplateData{
+	renders.RenderTemplate(w, r, "about.page.tmpl", &models.TemplateData{
 		StringMap: stringMap,
 	})
 }
 
 // Generals renders the room page
 func (m *Repository) Generals(w http.ResponseWriter, r *http.Request) {
-	renders.RenderTemplate(w,"generals.page.tmpl", &models.TemplateData{})
+	renders.RenderTemplate(w, r, "generals.page.tmpl", &models.TemplateData{})
 }
 
 // Majors renders the room page
 func (m *Repository) Majors(w http.ResponseWriter, r *http.Request) {
-	renders.RenderTemplate(w, "majors.page.tmpl", &models.TemplateData{})
+	renders.RenderTemplate(w, r, "majors.page.tmpl", &models.TemplateData{})
 }
 
 // Availability renders the search availability page
 func (m *Repository) Availability(w http.ResponseWriter, r *http.Request) {
-	renders.RenderTemplate(w, "search-availability.page.tmpl", &models.TemplateData{})
+	renders.RenderTemplate(w, r, "search-availability.page.tmpl", &models.TemplateData{})
+}
+
+// PostAvailability search availability page
+func (m *Repository) PostAvailability(w http.ResponseWriter, r *http.Request) {
+	start := r.Form.Get("start_date")
+	end := r.Form.Get("end_date")
+
+	w.Write([]byte(fmt.Sprintf("Start date is %s and end date is %s", start, end)))
+}
+
+type jsonResponse struct {
+	OK      bool `json:"ok"`
+	Message string `json:"message"`
+}
+
+// AvailabilityJSON handles request for availability and send JSON response
+func (m *Repository) AvailabilityJSON(w http.ResponseWriter, r *http.Request) {
+	resp := jsonResponse{
+		OK: true,
+		Message: "Available!",
+	}
+
+	out, err := json.MarshalIndent(resp, "", "     ")
+	if err != nil {
+		log.Println(err)
+	}
+
+	//log.Println(string(out))
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(out)
 }
 
 // Contact renders the contact page
 func (m *Repository) Contact(w http.ResponseWriter, r *http.Request) {
-	renders.RenderTemplate(w, "contact.page.tmpl", &models.TemplateData{})
+	renders.RenderTemplate(w, r, "contact.page.tmpl", &models.TemplateData{})
+}
+
+// Reservation renders the make-reservation page
+func (m *Repository) Reservation(w http.ResponseWriter, r *http.Request) {
+	renders.RenderTemplate(w, r, "make-reservation.page.tmpl", &models.TemplateData{})
 }
--- pkg/renders/renders.go
+++ pkg/renders/renders.go
@@ -2,6 +2,7 @@ package renders
 
 import (
 	"bytes"
+	"github.com/justinas/nosurf"
 	"github.com/tpatel84/golang-booking-web-dev/pkg/config"
 	"github.com/tpatel84/golang-booking-web-dev/pkg/models"
 	"html/template"
@@ -18,11 +19,12 @@ func NewTemplates(a *config.AppConfig) {
 	app = a
 }
 
-func AddDefaultData(td *models.TemplateData) *models.TemplateData {
+func AddDefaultData(td *models.TemplateData, r *http.Request) *models.TemplateData {
+	td.CSRFToken = nosurf.Token(r)
 	return td
 }
 
-func RenderTemplate(w http.ResponseWriter, tmpl string, td *models.TemplateData) {
+func RenderTemplate(w http.ResponseWriter, r *http.Request, tmpl string, td *models.TemplateData) {
 	var tc map[string]*template.Template
 
 	if app.UseCache {
@@ -46,7 +48,7 @@ func RenderTemplate(w http.ResponseWriter, tmpl string, td *models.TemplateData)
 
 	buf := new(bytes.Buffer)
 
-	td = AddDefaultData(td)
+	td = AddDefaultData(td, r)
 
 	_ = t.Execute(buf, td)
 	_, err := buf.WriteTo(w)
